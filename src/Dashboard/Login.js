8b23ca1af1ede2afdcd7b75fff468fc3e8d02872
chikun1 <bchikun.patra@ilearnplace.com>
2024-08-31 13:42:03 +0530

    update domain in all file

--- src/Dashboard/Login.js
+++ src/Dashboard/Login.js
@@ -16,7 +16,7 @@ const Login = () => {
     event.preventDefault();
     try {
       // Make POST request to Django backend API
-      const response = await axios.post('http://localhost:8000/login/', {
+      const response = await axios.post('https://gssprintinghub.in/login/', {
         contact: contact,
         password: password,
       });
